b52919df08cfce25a8a933ed8402291651e40727
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-09-10 02:44:44 +0300

    Implement About controller

--- src/Web/CleaningWeb.Web/Controllers/AboutController.cs
+++ src/Web/CleaningWeb.Web/Controllers/AboutController.cs
@@ -1,12 +1,27 @@
 ﻿namespace CleaningWeb.Web.Controllers
 {
+    using CleaningWeb.Services.Data.About;
+    using CleaningWeb.Web.ViewModels.About;
     using Microsoft.AspNetCore.Mvc;
 
     public class AboutController : BaseController
     {
+        private readonly IAboutService aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            this.aboutService = aboutService;
+        }
+
         public IActionResult Index()
         {
-            return this.View();
+            var viewModel = new AboutViewModel
+            {
+                BusinessViewModel = this.aboutService.GetBusinessViewModel<BusinessViewModel>(),
+                CompanyViewModel = this.aboutService.GetCompanyViewModel<CompanyViewModel>(),
+            };
+
+            return this.View(viewModel);
         }
     }
 }
