8f6a2df8f9d1512b27df5d93f78d86cd31f9b970
ViktorNikoloov <viktor.g.nikolov@gmail.com>
2021-09-10 04:28:44 +0300

    Change Layout UI  a little and add CompanyInfoViewModel

---
+++ src/Web/CleaningWeb.Web.ViewModels/Layout/CompanyInfoViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CleaningWeb.Web.ViewModels.Layout
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using CleaningWeb.Data.Models;
+    using CleaningWeb.Services.Mapping;
+
+    using static CleaningWeb.Common.DataGlobalConstants.Common;
+    using static CleaningWeb.Common.DataGlobalConstants.Company;
+
+    public class CompanyInfoViewModel : IMapFrom<Company>
+    {
+        [Required]
+        [MaxLength(AddressMaxLength)]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(PhoneMaxLength)]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
