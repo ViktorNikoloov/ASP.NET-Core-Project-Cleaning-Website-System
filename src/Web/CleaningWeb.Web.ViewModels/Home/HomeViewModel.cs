05a3a34f6b73e3ae7ea501ba4888f182c3418c70
Viktor Nikolov <viktor.g.nikolov@gmail.com>
2021-09-13 02:06:08 +0300

    Update
    marge test

--- src/Web/CleaningWeb.Web.ViewModels/Home/HomeViewModel.cs
+++ src/Web/CleaningWeb.Web.ViewModels/Home/HomeViewModel.cs
@@ -1,11 +1,9 @@
 ﻿namespace CleaningWeb.Web.ViewModels.Home
 {
     using CleaningWeb.Web.ViewModels.About;
-    using CleaningWeb.Web.ViewModels.Appointmen;
 
     public class HomeViewModel
     {
-        public AppointmentViewModel AppointmentViewModel { get; set; }
 
         public BusinessViewModel BusinessViewModel { get; set; }
 
